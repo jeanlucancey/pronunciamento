ea264a2dea36cdb6f247f11b2aaf3fbc8c01abec
Jean-Luc Ancey <triancey@gmail.com>
2018-12-03 23:48:14 +0100

    Refactorisation des URL

---
+++ cestmoilechef/urls.py
@@ -0,0 +1,55 @@
+from django.conf.urls import url
+
+from .views import (
+        pronunciamento, \
+        imagePorte, \
+        vignettes, \
+        exportePosts, \
+        creeCategories, \
+        listeCategories, \
+        creePhotos, \
+        listePhotos, \
+        purgePhotos, \
+        # listeCategories2, \ # Ligne neutralisee because shortcut
+        listeCategories4, \
+        # listePhotos2, \ # Ligne neutralisee because shortcut
+        # listePhotos3, \ # Ligne neutralisee because autre shortcut !
+        listePhotos4, \
+        categorie_detail_pabon, \
+        categorie_detail_pabon2, \
+        categorie_detail, \
+        # categorie_detail_shortcut, \ # Neutralise because shortcut once more...
+        categorie_detail_shortcut2, \
+        montrePhotoPrecise, \
+)
+
+urlpatterns = [
+    url(r'^cestmoilechef/',
+        pronunciamento,
+        name='pro_nun_cia_men_to'),
+    url(r'^image-porte/', imagePorte, name='ima_geport_te'),
+    url(r'^vignettes/', vignettes, name='vig_net_tes'),
+    url(r'^exporte-posts/', exportePosts, name='exp_ort_eposts'),
+    url(r'^cree-categories/', creeCategories, name='cree_cate_go_ries'),
+    url(r'^liste-categories/', listeCategories, name='liste_cate_gories'),
+    url(r'^cree-photos/', creePhotos, name='cree_pho_tos'),
+    url(r'^liste-photos/', listePhotos, name='liste_pho_tos'),
+    url(r'^purge-photos/', purgePhotos, name='purge_pho_tos'),
+    # url(r'^liste-categories2/', listeCategories3, name='liste_cate_gories_2'),
+    # La ligne precedente a été remplacée par la suivante, because shortcuts
+    url(r'^liste-categories2/', listeCategories4, name='liste_cate_gories_2'),
+    # url(r'^liste-photos2/', listePhotos2, name='liste_pho_tos_2'),
+    # La ligne precedente a été remplacée par la suivante, because shortcuts
+    url(r'^liste-photos2/', listePhotos4, name='liste_pho_tos_2'),
+    url(r'^categorie-pabon/[\w\-]+/$', categorie_detail_pabon, name='pabon'), # Pas recommande mais possible
+    url(r'^categorie-pabon2/(?P<slug>[\w\-]+)/$', categorie_detail_pabon2, name='pabon2'),
+    url(r'^categorie/(?P<slug>[\w\-]+)/$',
+        categorie_detail,
+        name='cestmoilechef_categorie_detail'), # p. 131 et 132 de Pinkham
+    url(r'^categorie-shortcut/(?P<slug>[\w\-]+)/$',
+        categorie_detail_shortcut2,
+        name='cestmoilechef_categorie_detail_shorcut'), # p. 137, puis 139 de Pinkham
+    url(r'^photo-precise/(?P<nomPhoto>[\w\-]+)/$',
+        montrePhotoPrecise,
+        name='montre_photo_precise'),
+]
--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -18,62 +18,14 @@ from django.contrib import admin
 
 from blog import urls as blog_urls
 from organizer import urls as organizer_urls
+from cestmoilechef import urls as cestmoilechef_urls
 
 from .views import redirect_root
-from cestmoilechef.views import (
-        pronunciamento, \
-        imagePorte, \
-        vignettes, \
-        exportePosts, \
-        creeCategories, \
-        listeCategories, \
-        creePhotos, \
-        listePhotos, \
-        purgePhotos, \
-        # listeCategories2, \ # Ligne neutralisee because shortcut
-        listeCategories4, \
-        # listePhotos2, \ # Ligne neutralisee because shortcut
-        # listePhotos3, \ # Ligne neutralisee because autre shortcut !
-        listePhotos4, \
-        categorie_detail_pabon, \
-        categorie_detail_pabon2, \
-        categorie_detail, \
-        # categorie_detail_shortcut, \ # Neutralise because shortcut once more...
-        categorie_detail_shortcut2, \
-        montrePhotoPrecise, \
-     )
 
 urlpatterns = [
     url(r'^$', redirect_root),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^blog/', include(blog_urls)),
-    url(r'^cestmoilechef/',
-        pronunciamento,
-        name='pro_nun_cia_men_to'),
-    url(r'^image-porte/', imagePorte, name='ima_geport_te'),
-    url(r'^vignettes/', vignettes, name='vig_net_tes'),
-    url(r'^exporte-posts/', exportePosts, name='exp_ort_eposts'),
-    url(r'^cree-categories/', creeCategories, name='cree_cate_go_ries'),
-    url(r'^liste-categories/', listeCategories, name='liste_cate_gories'),
-    url(r'^cree-photos/', creePhotos, name='cree_pho_tos'),
-    url(r'^liste-photos/', listePhotos, name='liste_pho_tos'),
-    url(r'^purge-photos/', purgePhotos, name='purge_pho_tos'),
-    # url(r'^liste-categories2/', listeCategories3, name='liste_cate_gories_2'),
-    # La ligne precedente a été remplacée par la suivante, because shortcuts
-    url(r'^liste-categories2/', listeCategories4, name='liste_cate_gories_2'),
-    # url(r'^liste-photos2/', listePhotos2, name='liste_pho_tos_2'),
-    # La ligne precedente a été remplacée par la suivante, because shortcuts
-    url(r'^liste-photos2/', listePhotos4, name='liste_pho_tos_2'),
-    url(r'^categorie-pabon/[\w\-]+/$', categorie_detail_pabon, name='pabon'), # Pas recommande mais possible
-    url(r'^categorie-pabon2/(?P<slug>[\w\-]+)/$', categorie_detail_pabon2, name='pabon2'),
-    url(r'^categorie/(?P<slug>[\w\-]+)/$',
-        categorie_detail,
-        name='cestmoilechef_categorie_detail'), # p. 131 et 132 de Pinkham
-    url(r'^categorie-shortcut/(?P<slug>[\w\-]+)/$',
-        categorie_detail_shortcut2,
-        name='cestmoilechef_categorie_detail_shorcut'), # p. 137, puis 139 de Pinkham
-    url(r'^photo-precise/(?P<nomPhoto>[\w\-]+)/$',
-        montrePhotoPrecise,
-        name='montre_photo_precise'),
     url(r'^', include(organizer_urls)),
+    url(r'^', include(cestmoilechef_urls)),
 ]
