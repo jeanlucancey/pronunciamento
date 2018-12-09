54cf7ef42df6cc22973617ce34227121d5e6ac8e
Jean-Luc Ancey <triancey@gmail.com>
2018-12-09 12:22:08 +0100

    Couillon que je suis, j'ai commité avant d'enregistrer!

--- cestmoilechef/forms.py
+++ cestmoilechef/forms.py
@@ -1,27 +1,19 @@
 from django import forms
 from django.core.exceptions import ValidationError
+
 from .models import Categorie
 
-class CategorieForm(forms.Form):
-    nomDsForm = forms.CharField(max_length=30)
-    slugDsForm = forms.SlugField(
-                     max_length=30,
-                     help_text='Une étiquette pour CategorieForm'
-                 )
+class CategorieForm(forms.ModelForm):
+    class Meta:
+        model = Categorie
+        fields = '__all__'
 
     def clean_nom(self):
     # S'assure que le nom est en bdc because ordre alpha informatique
-        return self.cleaned_data['nomDsForm'].lower()
+        return self.cleaned_data['nom'].lower()
 
     def clean_slug(self):
-        new_slug = (self.cleaned_data[slugDsForm].lower())
+        new_slug = (self.cleaned_data[slug].lower())
         if new_slug == 'create':
             raise ValidationError('Slug may not be "create".')
         return new_slug
-
-    def save(self):
-        new_categorie = Categorie.objects.create(
-                            nom = self.cleaned_data['nomDsForm'],
-                            slug = self.cleaned_data['slugDsForm']
-                        )
-        return new_categorie
